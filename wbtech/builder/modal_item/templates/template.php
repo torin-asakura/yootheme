fa512e3396206019b6f5454ffc418038dbcf6f39
Nikita Elfimov <me@tfk.name>
2022-12-16 22:48:49 +0500

    feat: grid, trigger, settings

--- wbtech/builder/modal_item/templates/template.php
+++ wbtech/builder/modal_item/templates/template.php
@@ -5,7 +5,7 @@
     <?php endif ?>
     <div class="uk-modal-dialog uk-modal-body">
         <?php if ($props['title']) : ?>
-        <<?= $props['title_element'] ?> class="uk-modal-title"><?= $props['title'] ?></<?= $props['title_element'] ?>>
+        <<?= $props['title_element'] ?> class="uk-modal-title <?= $props['title_color'] ? 'uk-text-'.$props['title_color'] : '' ?> <?= $props['title_style'] ? 'uk-title-'.$props['title_style'] : '' ?>"><?= $props['title'] ?></<?= $props['title_element'] ?>>
 <?php endif ?>
     <div class="uk-block"><?= $props['content'] ?></div>
 </div>
---
+++ wbtech/builder/modal_trigger/templates/template.php
@@ -0,0 +1,12 @@
+<?php if (!$props['status']) : ?>
+<a
+  uk-toggle
+  href="#<?= $props['modal_id'] ?>"
+  id="<?= $props['id'] ?>"
+  class="el-link uk-button <?= $props['button_style'] ? 'uk-button-'.$props['button_style'] : '' ?> <?= $props['class'] ?> <?= $props['button_size'] ? 'uk-button-'.$props['button_size'] : '' ?> <?= $props['fullwidth'] ? 'uk-width-1-1' : '' ?>"
+  style="<?= $props['css'] ?>"
+  <?= $props['attributes'] ?>
+>
+    <?= $props['content'] ?>
+</a>
+<?php endif ?>
