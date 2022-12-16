9ea3934b6a8c64699c1c6db94185c5f623505465
Andrew Ghostuhin <me@torinasakura.name>
2022-12-17 01:24:57 +0300

    chore: finalize modal stylization

--- wbtech/builder/modal_button/templates/template.php
+++ wbtech/builder/modal_button/templates/template.php
@@ -3,7 +3,7 @@
         uk-toggle
         href="#<?= $props['modal_id'] ?>"
         id="<?= $props['id'] ?>"
-        class="<?= $props['class'] ?>"
+        class="el-link uk-button <?= $props['button_style'] ? 'uk-button-'.$props['button_style'] : '' ?> <?= $props['class'] ?> <?= $props['button_size'] ? 'uk-button-'.$props['button_size'] : '' ?> <?= $props['fullwidth'] ? 'uk-width-1-1' : '' ?>"
         style="<?= $props['css'] ?>"
         <?= $props['attributes'] ?>
     >
