2ebd2022e7947a667e8b7969bfc13b3b7d6e1a46
Andrew Ghostuhin <me@torinasakura.name>
2022-12-16 11:19:51 +0300

    feat: finalizing modal component

--- wbtech/builder/modal/templates/template.php
+++ wbtech/builder/modal/templates/template.php
@@ -1,11 +1,3 @@
-<?php
-
-    $el = $this->el($props['modal_element']);
-
-?>
-
-<?= $el($props, $attrs) ?>
 <?php foreach ($children as $child) : ?>
-    <div uk-modal><?= $builder->render($child, ['element' => $props]) ?></div>
+    <?= $builder->render($child, ['element' => $props]) ?>
 <?php endforeach ?>
-<?= $el->end() ?>
