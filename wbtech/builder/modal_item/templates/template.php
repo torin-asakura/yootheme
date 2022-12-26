bbe5d2c1a21689c64b83f8e3f709f82ea2964653
Andrew Ghostuhin <me@torinasakura.name>
2022-12-27 00:35:10 +0300

    feat: add meta field to modal

--- wbtech/builder/modal_item/templates/template.php
+++ wbtech/builder/modal_item/templates/template.php
@@ -7,6 +7,9 @@
         <?php if ($props['title']) : ?>
             <<?= $props['title_element'] ?> class="uk-modal-title <?= $props['title_color'] ? 'uk-text-'.$props['title_color'] : '' ?> <?= $props['title_style'] ? 'uk-title-'.$props['title_style'] : '' ?>"><?= $props['title'] ?></<?= $props['title_element'] ?>>
         <?php endif ?>
+        <?php if ($props['meta']) : ?>
+            <<?= $props['meta_element'] ?> class="el-meta uk-text-meta uk-margin<?= $props['meta_margin'] ? '-'.$props['meta_margin'] : '' ?>-top <?= $props['meta_color'] ? 'uk-text-'.$props['meta_color'] : '' ?> <?= $props['meta_style'] ? 'uk-meta-'.$props['meta_style'] : '' ?>"><?= $props['meta'] ?></<?= $props['meta_element'] ?>>
+        <?php endif ?>
         <div class="uk-block"><?= $props['content'] ?></div>
     </div>
 </div>
