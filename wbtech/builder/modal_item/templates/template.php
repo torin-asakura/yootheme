6588a55ac43d7712029f757c6d99688435c08daf
Andrew Ghostuhin <me@torinasakura.name>
2022-12-30 23:23:20 +0300

    chore: added fullscreen layout

--- wbtech/builder/modal_item/templates/template.php
+++ wbtech/builder/modal_item/templates/template.php
@@ -3,14 +3,25 @@
         <?php if ($props['full_width']) : ?>
             <button class="uk-modal-close-default" type="button" uk-close></button>
         <?php endif ?>
-        <div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+        <div class="uk-modal-dialog uk-modal-body <?= $props['full_width'] ? 'uk-height-1-1' : '' ?>">
+
+        <?php if ($props['full_width']) : ?>
+            <div class="uk-position-center">
+        <?php endif ?>
+
         <?php if ($props['title']) : ?>
             <<?= $props['title_element'] ?> class="uk-modal-title <?= $props['title_color'] ? 'uk-text-'.$props['title_color'] : '' ?> <?= $props['title_style'] ? 'uk-title-'.$props['title_style'] : '' ?>"><?= $props['title'] ?></<?= $props['title_element'] ?>>
         <?php endif ?>
+
         <?php if ($props['meta']) : ?>
             <<?= $props['meta_element'] ?> class="el-meta uk-text-meta uk-margin<?= $props['meta_margin'] ? '-'.$props['meta_margin'] : '' ?>-top <?= $props['meta_color'] ? 'uk-text-'.$props['meta_color'] : '' ?> <?= $props['meta_style'] ? 'uk-meta-'.$props['meta_style'] : '' ?>"><?= $props['meta'] ?></<?= $props['meta_element'] ?>>
         <?php endif ?>
-        <div class="uk-container"><?= $props['content'] ?></div>
+
+        <?= $props['content'] ?>
+
+        <?php if ($props['full_width']) : ?>
+            </div>
+        <?php endif ?>
+        </div>
     </div>
-</div>
 <?php ?>
